f6a5b7149ec467ccec4b7e084b072878d6715e13
raihan <raihan2345@gmail.com>
2020-09-07 21:26:10 -0400

    adding functionality for linking category page and dashboard, also made some changes to css and flow

--- js/app.js
+++ js/app.js
@@ -22,3 +22,122 @@ const login = (event) => {
     document.getElementById("errorMsg").style.display = "block";
   }
 };
+
+const addNewCategory = () => {
+  event.preventDefault();
+  addCategoryToBudgetForm.style.display = "inline";
+  if (
+    addCategoryToBudgetForm.style.display === "inline" &&
+    addTransactionForm.style.display === "inline"
+  ) {
+    addTransactionForm.style.display = "none";
+  }
+};
+
+var selectedText = "";
+
+//when user selects category
+const unfoldTransactionForm = (selectedCategory) => {
+  event.preventDefault();
+  addTransactionForm.style.display = "inline";
+  //if (addCategoryToBudgetForm.style.display === "inline" && addTransactionForm.style.display === "inline") {
+  //    addCategoryToBudgetForm.style.display = "none";
+  //}
+  console.log(selectedCategory);
+  selectedText = selectedCategory;
+  var dataBaseString = window.localStorage.getItem("budgetAppData");
+  let database = JSON.parse(dataBaseString);
+  var exist = database.listofCategory.findIndex(
+    (x) => x.category == selectedText
+  );
+  if (exist != -1) {
+    document.getElementById("budgetInput").value =
+      database.listofCategory[exist].budget;
+  } else {
+    document.getElementById("budgetInput").value = 0;
+  }
+};
+// calling unfoldTransactionForm function when user clicks add new category
+const AddNewCategory = (selectedCategory) => {
+  //event.preventDefault();
+  //addTransactionForm.style.display = "inline";
+  //console.log(selectedCategory);
+  //selectedText = selectedCategory;
+  unfoldTransactionForm(selectedCategory);
+};
+
+let addTransactionForm = document.getElementById("addTransactionForm");
+let addCategoryToBudgetForm = document.getElementById(
+  "addCategoryToBudgetForm"
+);
+
+let expandListButton = document.querySelector("button");
+let foodButton = document.getElementById("food");
+let groceriesButton = document.getElementById("groceries");
+let billsButton = document.getElementById("bills");
+let entertainmentButton = document.getElementById("entertainment");
+
+//expandListButton.addEventListener("click", addNewCategory);
+//document.getElementById("row1").addEventListener("click", unfoldTransactionForm)
+
+const submitData = () => {
+  //if category not selected
+  if (selectedText == "" || selectedText == null) {
+    alert("select category");
+  } else {
+    var amount = document.getElementById("amountSpent").value;
+    var budget = document.getElementById("budgetInput").value;
+    if (amount && amount >= 0) {
+      var dataBaseString = window.localStorage.getItem("budgetAppData");
+      let database = JSON.parse(dataBaseString);
+      var exist = database.listofCategory.findIndex(
+        (x) => x.category == selectedText
+      );
+      if (exist != -1) {
+        var num1 = parseFloat(database.listofCategory[exist].used) || 0;
+        var num2 = parseFloat(amount) || 0;
+        var floatBudget = parseFloat(budget) || 0;
+        if (num1 + num2 > floatBudget) {
+          alert("Out of budget");
+        } else {
+          database.listofCategory[exist].used = num1 + num2;
+          database.listofCategory[exist].budget = floatBudget;
+          window.localStorage.setItem(
+            "budgetAppData",
+            JSON.stringify(database)
+          );
+          document.getElementById("amountSpent").value = 0;
+        }
+      } else {
+        var iconNew = document.getElementById(selectedText.toLowerCase());
+        if (iconNew != null) {
+          console.log(iconNew);
+          let newCat = {};
+          newCat.budget =
+            parseFloat(document.getElementById("budgetInput").value) || 0;
+          newCat.category = selectedText;
+          //common class added in css
+          newCat.class = "commonCategory";
+          newCat.src = iconNew.getAttribute("data-src");
+          newCat.used =
+            parseFloat(document.getElementById("amountSpent").value) || 0;
+          if (newCat.used > newCat.budget) {
+            alert("out of budget");
+            return;
+          }
+          console.log(newCat);
+          database.listofCategory.push(newCat);
+          window.localStorage.setItem(
+            "budgetAppData",
+            JSON.stringify(database)
+          );
+          document.getElementById("budgetInput").value = 0;
+        } else {
+          alert("Invalid Category");
+        }
+      }
+    } else {
+      alert("Enter Amount");
+    }
+  }
+};
