8683a85774d9e61fcf53b597cd4bb2e055f31512
austin <austinkandt@gmail.com>
2020-09-09 18:47:08 -0400

    Did some touch up

--- remove.js
+++ remove.js
@@ -1,13 +1,15 @@
-// Overlay/Modal hidden until list item is clicked
-document.querySelectorAll('.category').addEventListener('click', function() {
-    document.querySelector('.dark-overlay').style.display = 'block';
-});
+// Modal hidden until list item is clicked
+document.querySelectorAll('.category').forEach((item) => {
+    item.addEventListener('click', function() {
+      document.querySelector('.dark-overlay').style.display = 'block';
+    });
+  });
 
 
-// Cancel dismisses Overlay/Modal
+// Cancel button dismisses Modal
 document.querySelector('#remove-button-cancel').addEventListener('click', function() {
     document.querySelector('.dark-overlay').style.display = 'none';
 });
 
 
-// When list item is clicked,
+// When list item is clicked, save that particular ID and delete from list 
