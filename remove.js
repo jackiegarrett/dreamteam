44c68de71dc6549a433ea90750ee485722c98a34
austin <austinkandt@gmail.com>
2020-09-09 18:54:34 -0400

    Fixed the desktop styling

--- remove.js
+++ remove.js
@@ -1,13 +1,27 @@
-// Overlay/Modal hidden until list item is clicked
-document.querySelectorAll('.category').addEventListener('click', function() {
-    document.querySelector('.dark-overlay').style.display = 'block';
-});
+// // Overlay/Modal hidden until list item is clicked
+// document.querySelectorAll('.category').addEventListener('click', function() {
+//     document.querySelector('.dark-overlay').style.display = 'block';
+// });
 
 
-// Cancel dismisses Overlay/Modal
-document.querySelector('#remove-button-cancel').addEventListener('click', function() {
-    document.querySelector('.dark-overlay').style.display = 'none';
-});
+// // Cancel dismisses Overlay/Modal
+// document.querySelector('#remove-button-cancel').addEventListener('click', function() {
+//     document.querySelector('.dark-overlay').style.display = 'none';
+// });
 
 
 // When list item is clicked,
+
+
+// Modal hidden until list item is clicked
+document.querySelectorAll('.category').forEach((item) => {
+    item.addEventListener('click', function() {
+      document.querySelector('.dark-overlay').style.display = 'block';
+    });
+  });
+
+
+// Cancel button dismisses Modal
+document.querySelector('#remove-button-cancel').addEventListener('click', function() {
+    document.querySelector('.dark-overlay').style.display = 'none';
+});
